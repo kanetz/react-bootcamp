44aa949ba104c659f5a58f1d8840c519344f0c4c
Tong Zhang <tz.tong.zhang@hotmail.com>
2017-07-23 22:19:14 +0800

    add likes

--- web/scripts/components/photo.js
+++ web/scripts/components/photo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Card, Image} from 'semantic-ui-react';
+import {Card, Image, Icon} from 'semantic-ui-react';
 
 class Photo extends React.Component {
     render() {
@@ -10,6 +10,12 @@ class Photo extends React.Component {
                 <Image src={photo.url}/>
                 <Card.Content>
                     <Card.Header className="photo-description">{photo.description}</Card.Header>
+                </Card.Content>
+                <Card.Content extra>
+                    <a className="right floated">
+                        <Icon name='heart' />
+                        {photo.likes || 0} likes
+                    </a>
                 </Card.Content>
             </Card>
         );
@@ -21,6 +27,7 @@ Photo.propTypes = {
         id: PropTypes.number.isRequired,
         description: PropTypes.string,
         url: PropTypes.string,
+        likes: PropTypes.number,
     }).isRequired,
 };
 
