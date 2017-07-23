a9ac94577013daa1ddbd876555673fd9947dab54
Tong Zhang <tz.tong.zhang@hotmail.com>
2017-07-24 03:44:00 +0800

    more data

--- web/components/photo-album.js
+++ web/components/photo-album.js
@@ -54,10 +54,13 @@ function givenInitialState() {
         {description: 'AngularJS', url: 'https://angularjs.org/img/AngularJS-large.png'},
         {description: 'Ember.js', url: 'https://www.emberjs.com/images/learn/ember-4a021047.png'},
         {description: 'Meteor', url: 'http://docs.meteor.com/images/logo-coralspace-left.svg'},
+        {description: 'Babel', url: 'https://raw.githubusercontent.com/babel/logo/master/babel.png'},
+        {description: 'Webpack', url:'https://camo.githubusercontent.com/d18f4a7a64244f703efcb322bf298dcb4ca38856/68747470733a2f2f7765627061636b2e6a732e6f72672f6173736574732f69636f6e2d7371756172652d6269672e737667'},
+        {description: 'PostCSS', url: 'http://postcss.org/_/web_modules/Hero/postcss.svg'},
     ].map((photo, index) => ({
         ...photo,
         id: index + 1,
-        likes: Math.floor(Math.random() * 10000),
+        likes: Math.floor(100 + Math.random() * 900),
     }));
     return { photos };
 }
